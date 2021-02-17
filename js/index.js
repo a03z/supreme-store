a9e7bcf3318fda2ec0493860ea51158da790efef
Daniil a03z Smeloff <hovickishow@gmail.com>
2021-02-17 13:56:21 +0300

    modals added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 const contactBtn = document.getElementById('contactBtn')
 const contactInfo = document.getElementById('contactInfo')
+const container = document.getElementById('container')
 
 contactBtn.addEventListener('click', () => {
     contactInfo.classList.toggle('active')
+})
+
+container.addEventListener('click', () => {
+    if (contactInfo.classList.contains('active')) {
+        contactInfo.classList.remove('active')
+    }
 })
