54974f8b25304dad3dd6e3d3e7fdd2b6edc95698
Daniil a03z Smeloff <hovickishow@gmail.com>
2021-02-22 15:46:51 +0300

    major updates
    added footer, order page, minor fixes
    TODO: change btc, card and gamemoney icons

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const cardMethod = document.getElementById('method-card')
+const gmoneyMethod = document.getElementById('method-gmoney')
+const btcMethod = document.getElementById('method-btc')
+
+cardMethod.addEventListener('click', () => {
+    cardMethod.classList.add('order-active')
+    gmoneyMethod.classList.remove('order-active')
+    btcMethod.classList.remove('order-active')
+})
+btcMethod.addEventListener('click', () => {
+    btcMethod.classList.add('order-active')
+    gmoneyMethod.classList.remove('order-active')
+    cardMethod.classList.remove('order-active')
+})
+gmoneyMethod.addEventListener('click', () => {
+    gmoneyMethod.classList.add('order-active')
+    btcMethod.classList.remove('order-active')
+    cardMethod.classList.remove('order-active')
+})
