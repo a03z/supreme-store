5eb039abe74d22fc1df4cc8a8e91311eec894e7d
Daniil a03z Smeloff <hovickishow@gmail.com>
2021-02-23 23:45:20 +0300

    styles changes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const cardMethod = document.getElementById('method-card')
 const gmoneyMethod = document.getElementById('method-gmoney')
 const btcMethod = document.getElementById('method-btc')
-
 cardMethod.addEventListener('click', () => {
     cardMethod.classList.add('order-active')
     gmoneyMethod.classList.remove('order-active')
@@ -17,3 +16,14 @@ gmoneyMethod.addEventListener('click', () => {
     btcMethod.classList.remove('order-active')
     cardMethod.classList.remove('order-active')
 })
+// !жалкие попытки отсортировать)))
+// let accountList = document.querySelectorAll('.account')
+// let accountListArr = Array.prototype.slice.call(accountList)
+// accountListArr[0].firstElementChild.innerText = parseInt(
+//     accountListArr[0].firstElementChild.innerText
+// )
+// accountListArr = accountListArr.sort((a, b) => {
+//     parseInt(a.firstElementChild.innerText) -
+//         parseInt(b.firstElementChild.innerText)
+//     console.log(parseInt(a.firstElementChild.innerText))
+// })
